e0a91e4a5136d3c74d5981d32090a666eb6f74d2
Leon <tahjib.leon399@gmail.com>
2024-04-28 20:57:41 +0600

    useInterval hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import './App.css'
-import useIsOnline from './hooks'
+import { useInterval, useIsOnline, useMousePointer } from './hooks'
 
 function App() {
   const isOnline = useIsOnline();
-  console.log(isOnline);
+  const mousePosition = useMousePointer();
+  const [count, setCount] = useState(0);
+
+  useInterval(() => {
+    setCount(c => c + 1)
+  }, 1000)
   return (
     <div>
       <h1>Custom Hooks</h1>
-      {isOnline ? <div>You are online</div> : <div>You are offline</div>}
+      {isOnline ? <div>You are online</div> : <div>You are offline</div>} <br />
+      <p>Your mouse position is: {mousePosition.x} & {mousePosition.y}</p>
+      <p>useInterval hook: {count}</p>
     </div>
   )
 }
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-export default function useIsOnline() {
+//TODO: Create a hook that returns true or false based on weithers the user is currently online
+export function useIsOnline() {
     const [isOnline, setIsOnline] = useState(window.navigator.onLine);
 
     useEffect(() => {
@@ -9,4 +10,34 @@ export default function useIsOnline() {
     }, [])
 
     return isOnline;
+}
+
+
+//TODO: Create a hook that returns you the current mouse pointer position.
+export const useMousePointer = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const handleMouseMove = (e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    useEffect(() => {
+        window.addEventListener('mousemove', handleMouseMove);
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    return position;
+};
+
+
+//TODO: Create a hook that runs a certain callback function every n seconds
+export const useInterval = (callback, delay) => {
+    return useEffect(() => {
+        const id = setInterval(callback, delay);
+
+        //cleanup fn
+        return () => clearInterval(id);
+    }, [callback, delay])
 }
