2821783557932f348ed2f896fe35af861df4be56
Leon <tahjib.leon399@gmail.com>
2024-04-28 21:23:27 +0600

    useDebounce hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
-import { useInterval, useIsOnline, useMousePointer } from './hooks'
+import { useDebounce, useInterval, useIsOnline, useMousePointer } from './hooks'
+import axios from 'axios';
 
 function App() {
   const isOnline = useIsOnline();
+  //-----------------
   const mousePosition = useMousePointer();
+  // --------useInterval
   const [count, setCount] = useState(0);
-
+  //------------useDebounce
+  const [inputValue, setInputValue] = useState('');
+  const debouncedValue = useDebounce(inputValue, 2000); // 500 milliseconds debounce delay
+  // Use the debouncedValue in your component logic, e.g., trigger a search API call via a useEffect
+  useEffect(() => {
+    console.log(debouncedValue)
+    console.log("debounce");
+  }, [debouncedValue]);
+  //__________________useInterval hook__________________________________
   useInterval(() => {
     setCount(c => c + 1)
-  }, 1000)
+  }, 1000);
+
+
   return (
     <div>
       <h1>Custom Hooks</h1>
       {isOnline ? <div>You are online</div> : <div>You are offline</div>} <br />
       <p>Your mouse position is: {mousePosition.x} & {mousePosition.y}</p>
       <p>useInterval hook: {count}</p>
+      useDebounce hook :
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Search..."
+      />
     </div>
   )
 }
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -41,3 +41,21 @@ export const useInterval = (callback, delay) => {
         return () => clearInterval(id);
     }, [callback, delay])
 }
+
+//TODO: Create a hook that debounces a value given
+export const useDebounce = (value, delay) => {
+    // State to store the debounced value
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    //solutiuon 2
+    useEffect(() => {
+        // Set up a timer to update the debounced value after the specified delay
+        const timerId = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        // Clean up the timer if the value changes before the delay has passed
+        return () => clearTimeout(timerId);
+    }, [value, delay]);
+
+    return debouncedValue;
+}
